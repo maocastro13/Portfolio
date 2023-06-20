73af33b4d7b140d126b19d818a50e063cc89238b
maocastro13 <maocastro13@gmail.com>
2023-06-20 00:29:33 -0500

    Responsive Update and animation cicle added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
-const Hero = () => {
+const Hero = ({ isMobile }) => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
@@ -27,9 +27,9 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-start items-end'>
         <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-start items-start p-2'>
             <motion.div
               animate={{
                 y: [0, 24, 0],
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,26 +11,62 @@ const Computers = ({ isMobile }) => {
   const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
-    actions[names[index]]?.reset().fadeIn(0.5).play();
-
+    const animationName = names[index];
+    const animationAction = actions[animationName];
+  
+    if (animationAction) {
+      animationAction.reset().fadeIn(0.5).play();
+      
+      const maxIterations = 4; // Número máximo de iteraciones
+      const animationDuration = animationAction.getClip().duration;
+      const framesPerSecond = 100; // Cuadros por segundo (ajusta este valor)
+      const framesPerIteration = Math.floor(framesPerSecond * animationDuration);
+      const frameDuration = 1 / framesPerSecond;
+      const iterationDuration = framesPerIteration * frameDuration;
+      
+      let currentIteration = 0;
+      let frame = 0;
+  
+      const animate = () => {
+        if (currentIteration < maxIterations) {
+          const time = currentIteration * iterationDuration + frame * frameDuration;
+          animationAction.time = time % animationDuration;
+          animationAction.paused = false;
+          frame++;
+    
+          if (frame >= framesPerIteration) {
+            frame = 0;
+            currentIteration++;
+          }
+    
+          requestAnimationFrame(animate);
+        } else {
+          animationAction.fadeOut(0.5);
+        }
+      };
+  
+      animate();
+    }
+  
     return () => {
-      actions[names[index]]?.fadeOut(0.5);
+      if (animationAction) {
+        animationAction.fadeOut(0.5);
+      }
     };
   }, [index, actions, names]);
-
+  
   return (
     <group>
     <primitive
       object={computer.scene}
-      scale={2}
-      position-y={-2.5}
-      rotation-y={-0.7}
-      position-x={[4.5]}
+      scale={isMobile ? 2 : 2}
+      position={isMobile ? [0.2, -4.7, -2] : [4.5, -2.5, 0.1]}
+      rotation={isMobile ? [0, 0, 0] : [0.1, -0.7, 0]}
     />
 
-    <Html position={[-4.4, 0, 0]}>
+    <Html position={isMobile ? [1, -2, 0] : [-4.4, -1, 0]}>
       <button
-        className="font-black text-[#915EFF] lg:text-[60px] sm:text-[40px] xs:text-[30px] text-[20px] lg:leading-[98px] mt-2 w-[100px] p-2 rounded-lg text-xs sm:text-lg sm:w-[500px] hover:bg-white hover:scale-110 duration-500"
+        className="font-black text-white-100 lg:text-[60px] sm:text-[40px] xs:text-[30px] text-[20px] lg:leading-[98px] mt-2 w-[100px] p-2 rounded-lg text-xs sm:text-lg sm:w-[500px] hover:bg-violet-700 hover:scale-110 duration-500"
         onClick={() => {
           setIndex((index + 1) % names.length);
           setIsClicked(!isClicked);
