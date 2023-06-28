1dcd80f60fcf296858262fce7b0eede225255f15
maocastro13 <maocastro13@gmail.com>
2023-06-27 23:48:07 -0500

    Fixed Experience text for mobile context7

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -59,14 +59,14 @@ const ExperienceCard = ({ experience }) => {
 const Experience = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider text-center">
+      <div>
+        <p className={`${styles.sectionSubText} text-center`}>
           In what I have worked so far,
         </p>
-        <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
+        <h2 className={`${styles.sectionHeadText} text-center`}>
           Experience.
         </h2>
-      </motion.div>
+      </div>
 
       <div className='mt-20 flex flex-col'>
         <VerticalTimeline>
